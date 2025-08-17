377e21d063f991d2dfc3def1243bff901c1e8e99
Braian Silva <braiansilva4@gmail.com>
2025-08-17 01:22:54 -0300

    Se muestran nombres y apellidos de las personas cargadas al archivo json

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL)
+.then(res => res.json())
+.then(data => {
+  showData(data.students);
+})
+
+.catch(error => {
+  console.error("Error al obtener datos", error);
+});
